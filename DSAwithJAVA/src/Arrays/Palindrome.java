59c7d2c6087194dd27e2781525be7388837fac0a
Aditya <adityasingh62065@gmail.com>
2023-04-20 08:26:28 +0530

    palidromic array question solved

--- DSAwithJAVA/src/Arrays/Palindrome.java
+++ DSAwithJAVA/src/Arrays/Palindrome.java
@@ -2,6 +2,19 @@ package Arrays;
 
 public class Palindrome {
     public static void main(String[] args) {
-        int [] array = {1,2,3,2,1};
+        int array[] = {1, 2, 3, 2, 1,4};
+        int n = array.length;
+        int flag = 0;
+        for (int i = 0; i < n/2; i++) {
+            if(array[i] != array[n-i-1])
+            {
+                flag = 1;
+                System.out.println("It's not a palidromic array");
+                break;
+            }
+            if (flag == 0) {
+                System.out.println("palidromic array");
+            }
+        }
     }
 }
