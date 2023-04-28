5e482e0e41b5a6e9c84504a312e0d3c1c6359e48
Aditya <adityasingh62065@gmail.com>
2023-04-28 13:40:03 +0530

    till bubble sort completed

---
+++ DSAwithJAVA/src/Sorting/BubbleSort.java
@@ -0,0 +1,33 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] arr)
+    {
+        for (int i = 0; i < arr.length; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < arr.length-i-1; j++) {
+                if(arr[j]>arr[j+1])
+                {
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    swapped = true;
+                }
+            }
+            if(!swapped)
+            {
+                break;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {12,45,90,23,13,78,3};
+        //function calling
+        bubbleSort(arr);
+        System.out.println("Sorted Array is : ");
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
