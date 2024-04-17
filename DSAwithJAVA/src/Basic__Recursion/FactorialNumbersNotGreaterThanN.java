2428128f20aa40d50368293a33dd8ba06f0f4c65
Aditya <adityasingh62065@gmail.com>
2024-04-17 13:04:38 +0530

    add problem public class FactorialNumbersNotGreaterThanN {

---
+++ DSAwithJAVA/src/Basic__Recursion/FactorialNumbersNotGreaterThanN.java
@@ -0,0 +1,32 @@
+package Basic__Recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FactorialNumbersNotGreaterThanN {
+    public static long findFactorial(long n)
+    {
+        if(n==0)
+        {
+            return 1;
+        }
+        return n*findFactorial(n-1);
+    }
+    public static List<Long> factorialNumbers(long n){
+        ArrayList<Long> ans = new ArrayList<>();
+        int p=1;
+        while (findFactorial(p)<n)
+        {
+            ans.add((long)findFactorial(p));
+            p++;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+       List<Long> ans = factorialNumbers(7);
+        for (long n:ans ) {
+            System.out.println(n);
+        }
+
+    }
+}
