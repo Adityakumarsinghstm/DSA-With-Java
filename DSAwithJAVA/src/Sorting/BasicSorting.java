027ebe217967d2cab8fbe3ae72dcb2ab43106483
Aditya <adityasingh62065@gmail.com>
2024-04-30 11:33:58 +0530

    add count sort

--- DSAwithJAVA/src/Sorting/BasicSorting.java
+++ DSAwithJAVA/src/Sorting/BasicSorting.java
@@ -43,6 +43,27 @@ public class BasicSorting {
             arr[prev+1] = curr;
         }
     }
+    public static void countingSort(int[] arr)
+    {
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i <arr.length ; i++) {
+            largest = Math.max(largest,arr[i]);
+        }
+
+        int[] count = new int[largest+1];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+        int j = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i]>0)
+            {
+                arr[j]=i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
     public static void printArr(int[] arr)
     {
         for (int i = 0; i < arr.length; i++) {
@@ -51,10 +72,11 @@ public class BasicSorting {
         System.out.println();
     }
     public static void main(String[] args) {
-        int[] arr = {1,8,6,9,4,2,0};
+        int[] arr = {1,8,6,9,4,2,0,2,4,2,3};
         //bubbleSort(arr);
         //selectionSort(arr);
-        insertionSort(arr);
+        //insertionSort(arr);
+        countingSort(arr);
         printArr(arr);
     }
 }
